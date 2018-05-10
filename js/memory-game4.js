1dd16217ba7ddf3cd478019ce4aeea88af3728b1
Barak Fisher <barakfisher@gmail.com>
2018-05-10 12:16:04 +0300

    added an ugly modal for now, starting with stopwatch

--- js/memory-game4.js
+++ js/memory-game4.js
@@ -27,13 +27,14 @@ function boardBuild() {
     imagesArr = getImageArr(); // will return an array filled with images reffs (index 0 is 1)
     for (var i = 0; i < board.amountOfCards; i++) {
         var card = document.createElement("div");
-        card.classList.add("cards");
+        card.classList.add("cards","clickable");
         card.setAttribute("location", imagesArr[i]);
         card.style.width = 100 / Math.sqrt(board.amountOfCards) + "%";
         card.style.height = 100 / Math.sqrt(board.amountOfCards) + "%";
         card.addEventListener("click", flip);
         theBoard.appendChild(card);
     }
+    document.getElementById("victory").style.display = "none";
 }
 
 function flip(event) {
@@ -61,12 +62,6 @@ function flip(event) {
             board.currentFlippedCards = 0;
         }
     }
-    // if(player.rightGuesses == board.amountOfCards/2 ){
-    //     // zeroAll();
-    //     debugger;
-    //     document.getElementById("victory").style.display = block;
-
-    // }
 }
 function emptyArray(arr) {
     for (var i = 0; i < arr.length; i++) {
@@ -113,9 +108,12 @@ function getSelectedLevel() {
     board.level = document.getElementById("selectLevel").value;
     board.amountOfCards = getItemNumber(document.getElementById("selectLevel").value);
     boardBuild();
-    
 }
 document.getElementById("selectLevel").addEventListener("change", getSelectedLevel);
+document.getElementsByClassName("exit-img")[0].addEventListener("click", function(){
+    document.getElementById("victory").style.display ="none";
+});
+
 
 function getItemNumber(str) {
     len = str.length;
@@ -150,3 +148,50 @@ function getNumbersArr() {
     }
     return numbersArr.concat(numbersArr);
 }
+
+
+
+stopWatch = {
+            seconds: 0,
+            minutes: 0,
+            cond: true,
+            status: true
+        }
+        document.getElementById("start").addEventListener("click", toStart);
+        document.getElementById("stop").addEventListener("click", toZero);
+
+
+        function secondsCount() {
+            function test() {
+                if (stopWatch.cond == true) {
+                    if (stopWatch.seconds == 59) {
+                        stopWatch.seconds = 0;
+                        stopWatch.minutes++;
+                    }
+                    if (stopWatch.minutes == 59) {
+                        stopWatch.minutes = 0;
+                    }
+                    stopWatch.seconds++;
+                    document.getElementById("dateId").innerHTML =  stopWatch.minutes+ ":" + stopWatch.seconds;
+                } else {
+                    clearInterval(x);
+                }
+            }
+               var x= setInterval(test, 1000);
+        }
+
+        function toZero() {
+            stopWatch.seconds = 0;
+            stopWatch.minutes = 0;
+            stopWatch.cond = false;
+            stopWatch.status = true;
+            secondsCount();
+        }
+        function toStart() {
+            if(stopWatch.status===true){
+                stopWatch.status = false;
+                stopWatch.cond = true;
+                secondsCount();
+            }
+        }
+
